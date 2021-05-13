84471ece3894835af0542f088380c3867d9be29c
Sercan <ssercanozerr@gmail.com>
2021-05-13 05:07:00 +0300

    cv.php

---
+++ cv.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/cv.css">
+    <title>CV</title>
+  </head>
+  <body>
+    <header>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+    </header>
+
+    <h2 class="mb-5 text-center">Kişisel Bilgiler</h2>
+    <table class="table table-dark table-striped">
+        <tbody>
+          <tr>
+            <td>Ad Soyad: Sercan ÖZER</td>            
+          </tr>
+          <tr>            
+            <td>Doğum Tarihi: 19/02/2001</td>
+          </tr>
+          <tr>            
+            <td>Doğum Yeri: Mardin</td>
+          </tr>
+        </tbody>
+    </table>
+
+      <h2 class="mb-5 text-center">Eğitim Bilgileri</h2>
+      <table class="table table-dark table-striped">
+        <tbody>
+          <tr>
+            <td>2015-2019: Mardin Anadolu Lisesi</td>           
+          </tr>
+          <tr> 
+            <td>2012-2015: Tatlıdede Ortaokulu</td>
+          </tr>
+          <tr>            
+            <td>2008-2012: Abdulkadir Tutaşu İlköğretim Okulu</td>
+          </tr>
+        </tbody>
+    </table>
+
+    <h2 class="mb-5 text-center">Bilgisayar Bilgileri</h2>
+    <table class="table table-dark table-striped">
+      <tbody>
+        <tr>
+          <td>Programlama Dilleri: C++, C#</td>         
+        </tr>
+        <tr> 
+          <td>İşletim Sistemi: Windows</td>
+        </tr>        
+      </tbody>
+  </table>
+
+  <h2 class="mb-5 text-center">İletişim Bilgileri</h2>
+  <table class="table table-dark table-striped">
+    <tbody>
+      <tr>
+        <td>Telefon: +90 535 432 56 68</td>            
+      </tr>
+      <tr> 
+        <td>Adres: 13 Mart Mahallesi 23. Sokak Ferahkent C Blok Kat:4 No:15</td>
+      </tr>        
+    </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <footer>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+    </footer>
+  </body>
+</html>
