431ee3973ad408bbb01dae95aecdba093f7a691a
Sercan <ssercanozerr@gmail.com>
2021-05-15 01:24:24 +0300

    sehir.php

---
+++ sehir.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/sehir.css">
+    <title>Şehir</title>
+  </head>
+  <body>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="10" aria-label="Slide 11"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="11" aria-label="Slide 12"></button>
+        </div>
+
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <a href="https://www.google.com/search?q=Mardin+Kalesi">
+            <img src="https://pbs.twimg.com/media/B-MlbGGCEAApryS.png" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>    
+        </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=deyrulzafaran+manast%C4%B1r%C4%B1">
+            <img src="https://www.atlasdergisi.com/wp-content/uploads/2020/01/mardin-17.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=zinciriye+medresesi">
+            <img src="https://hcrtyldz47.files.wordpress.com/2015/04/img_0272__acds__x800.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=mor+behnam+(k%C4%B1rklar)+kilisesi">
+            <img src="https://merakligezgindot.files.wordpress.com/2018/04/0x0-001.jpg?w=1100" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=kas%C4%B1miye+medresesi">
+            <img src="https://www.mardingzt.com/wp-content/uploads/2020/11/Kasimiye-Medresesi.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=mardin+ulu+camii">
+            <img src="https://www.grafimx.com/img.php?src=foto/u10/2014-11-30cemilsipahi01067f6f4ec8b1a.jpg&w=1140" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=dara+antik+kenti">
+            <img src="https://www.rehbername.com/timthumb.php?src=/uploads/images/custom/296-kapak1.jpg&w=1110&h=467&zc=1" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=mardin+m%C3%BCzesi">
+            <img src="https://www.gidilmeli.com/sayfa/resim/f48a7ca9544584fa22978e97b1f8f946.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=sabanc%C4%B1+mardin+kent+m%C3%BCzesi">
+            <img src="https://gezicini.com/wp-content/uploads/2020/11/02-8.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=meryemana+kilisesi">
+            <img src="https://www.kulturportali.gov.tr/repoKulturPortali/large/SehirRehberi//GezilecekYer/20191125110544781_Diyarbakir%20MeryemanaKilisesi%20Mehmet%20Kilicoglu%2051.jpg?format=jpg&quality=50" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=mor+gabriel+manast%C4%B1r%C4%B1">
+            <img src="https://gazetekarinca.com/wp-content/uploads/2017/06/5f5b8ca5-4cc3-4e3d-b4f8-8e9856b5aa96.jpg" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="https://www.google.com/search?q=mardin+evleri">
+            <img src="https://i4.hurimg.com/i/hurriyet/75/770x0/5bfb9f9fc03c0d14b425309b" class="d-block w-100 resim" alt="Mardin Resmi/slider"></a>
+          </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <section class="p-5 text-center">
+        <h2 class="mb-5 text-center"> Şehir Hakkında</h2>
+        <p>Mardin, mimari, etnografik, arkeolojik, tarihi ve görsel değerleri ile zamanın durduğu izlenimini veren Güneydoğunun şiirsel kentlerinden biridir. 
+            Mardin'de, farklı dini inanışlar paralelinde, sanatsal açıdan da tarihi değeri olan camiler, türbeler, kiliseler, manastır ve benzeri dini eserler barındırmaktadır.
+            Mardin ilinin yüzölçümü: 8.891 km² nüfusu ise  861.364'tür. Mardin'in trafik numarası 47'dir.</p>
+    </section>
+
+    <div id="harita"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d286798.4129714278!2d40.80941195437203!3d37.420453439749465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
+            1s0x400a61c7329e4703%3A0xef2490685395b5e2!2sMardin!5e0!3m2!1str!2str!4v1620228300758!5m2!1str!2str" width="600" height="715" style="border:0;" allowfullscreen="" loading="lazy">
+        </iframe>
+    </div>
+
+    <ul id="gezi"><h2>Gezilecek Yerler</h2><br>
+        <li>Mardin Kalesi</li><br>
+        <li>Deyrulzafaran Manastırı</li><br>
+        <li>Zinciriye Medresesi</li><br>
+        <li>Mor Behnam (Kırklar) Kilisesi</li><br>
+        <li>Kasımiye Medresesi</li><br>
+        <li>Mardin Ulu Camii</li><br>
+        <li>Mardin Dara Antik Kenti</li><br>
+        <li>Mardin Müzesi</li><br>
+        <li>Sabancı Mardin Kent Müzesi</li><br>
+        <li>Meryemana Kilisesi ve Patrikhanesi</li><br>
+        <li>Mor Gabriel Manastırı</li><br>
+        <li id="gezi-alt">Mardin Evleri</li>
+    </ul>
+
+    <ul id="ilçe"><h2>İlçeler</h2><br>
+        <li>Artuklu</li><br>
+        <li>Dargeçit</li><br>
+        <li>Derik</li><br>
+        <li>Kızıltepe</li><br>
+        <li>Mazıdağı</li><br>
+        <li>Midyat</li><br>
+        <li>Nusaybin</li><br>
+        <li>Ömerli</li><br>
+        <li>Savur</li><br>
+        <li id="ilçe-alt">Yeşilli</li>
+    </ul>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+  </body>
+</html>
