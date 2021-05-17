1cd6715d3f3973f54f48e3396ccb289d45fcda60
Sercan <ssercanozerr@gmail.com>
2021-05-17 11:56:39 +0300

    miras.php

---
+++ miras.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="tr">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/miras.css">  
+      <title>Şehrin Mirası</title>
+    </head>
+    <body>
+      <header>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+      </header>
+
+      <section class="p-5 text-center">
+        <h2 class="mb-5 text-center">Ulu Camii</h2>
+        <p>Artuklu Dönemi mimari örneklerinden, dilimli kubbesi ve minaresiyle Mardin’in sembolü olan Mardin Ulu Cami kayıtlara göre iki minareli inşa edilmiştir. 
+        Caminin bugün mevcut olan tek minaresinin kare kaidesindeki yazıt, yapım tarihini 1176 olarak vermektedir, fakat bugünkü minare 1888/1889 yıllarında yeni ve elektik bir üslupla yapılmıştır. 
+        Bazı Süryani yazarlar kiliseden çevrildiğini söylerler. Yapı kiliseden çevrilmemiş olsa bile, yerinde eski bir kilisenin bulunması muhtemeldir.</p> 
+          
+        <p>Yapı 12'nci yüzyıl Artuklu Dönemi mimarisinin temel özelliklerini yansıtır. 
+        Erken dönemde özellikle güneydoğuda meydana çıkan, mihrap önü kubbeli enine gelişen cami plan ve formunun çok önemli bir örneğidir. 
+        Yapının malzemesi düzgün kesme taştır. Ulu Cami’nin kubbesi dıştan yivleme tekniğiyle yapılmıştır. 
+        İlk olarak bu binada kullanılmış ve sonraları Mardin’de gelenek halini almıştır ki bazı geç dönem Artuklu yapılarında karakteristiktir. 
+        Caminin dikdörtgen avlusu kuzeyinde kalır. Avlunun güneyinde mihrap duvarına paralel, beşik tonozlu üç neften oluşan, mihrap duvarına yakın iki nefin kubbe ile kesildiği, enine gelişmiş, mihrap önü kubbeli bir şema görülür. 
+        Bu şema, aynı zamanda, çevredeki birçok yapı tarafından taklit edilmiş bir modeldir.</p>
+      </section>
+        
+      <a href="https://www.google.com/search?q=mardin+ulu+camii&rlz=1C1CHZN_trTR919TR919&sxsrf=ALeKk00DunAWHF9gK9vICiN26T0Wn-u7vg:1620244885027&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj-tsjGqrPwAhWY7aQKHesvAAUQ_AUoAXoECAEQAw&cshid=1620245121546483&biw=1920&bih=937#imgrc=6r689hKzEtxXJM">
+        <img src="https://www.grafimx.com/img.php?src=foto/u5/2014-08-19cemilsipahi1a3a37cfe734e06.jpg&w=1140" class="img-fluid"></a>
+
+      <div id="harita"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3173.267596636453!2d40.735086215583316!3d37.312488546393524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x400a89e60b88d711%3A0xb68ee014329b9212!2sMardin%20Ulucamii!5e0!3m2!1str!2str!4v1620246742394!5m2!1str!2str" width="700" height="767" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+
+      <footer>
+        <div>&nbsp;</div>
+        <div>Hazırlayan: Sercan ÖZER</div>
+        <div>&nbsp;</div>
+      </footer>
+    </body>
+</html>
