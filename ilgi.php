95ed6018e9232e652aac1118294b07f95b75959e
Sercan <ssercanozerr@gmail.com>
2021-05-22 17:48:46 +0300

    ilgi.php

---
+++ ilgi.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/ilgi.css">
+    <title>Hava Durumu</title>
+  </head>
+  <body>
+    <div class="degerler">
+      <div class="baslik">
+        <h1>Hava Durumu</h1>
+        <input type="text" id="searchBar" placeholder="Şehir Giriniz...">
+      </div>
+      <div class="content">
+        <div class="sehir">Mardin, TR</div>
+        <div class="derece">23°C</div>
+        <div class="durum">açık</div>
+        <div class="hissedilen">20°c / 23°c</div>
+      </div>
+    </div>
+    <script src="ilgi.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
