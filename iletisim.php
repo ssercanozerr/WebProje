e726d233d7e76ebb362802aea2eff46b9f99d456
Sercan <83230914+ssercanozerr@users.noreply.github.com>
2021-05-22 20:32:59 +0300

    iletisim.php

--- iletisim.php
+++ iletisim.php
@@ -15,6 +15,7 @@
         <div>&nbsp;</div>
         <div>&nbsp;</div>
     </header>
+    
     <form action="bilgi.php" method="POST">
         <fieldset>
             <legend><h1>İletişim</h1></legend>
@@ -88,7 +89,9 @@
             </label>
         </fieldset>
     </form>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    
     <footer>
         <div>&nbsp;</div>
         <div>&nbsp;</div>
