cf49505ca50f11749972a2e7cac5912f61e58db5
Sercan <ssercanozerr@gmail.com>
2021-05-22 17:49:28 +0300

    anasayfa.php

---
+++ anasayfa.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/anasayfa.css">
+    <title>Ana Sayfa</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Menü</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#hakkımda">Hakkımda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="cv.php">CV</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="sehir.php">Şehir</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="miras.php">Şehrin Mirası</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ilgi.php">İlgi Alanlarım</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="giris.php">Giriş Yap</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="iletisim.php">İletişim</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+    <section class="p-5 text-center">
+      <h2 class="mb-5 text-center">Hakkımda</h2>
+      <p id="hakkımda">Adım Sercan soyadım ÖZER. 20 yaşındayım. Mardinliyim. Mardin Anadolu Lisesi mezunuyum. Sakarya Üniversitesi
+      Bilgisayar Mühendisliği Bölümünde 1. Sınıf öğrencisiyim.</p>
+
+      <p>Hobilerim: Satranç oynamak, dizi ve film izlemek, futbol oynamak, kitap okumak,
+      tenis oynamak, yüzmek, bilgisayar oyunu oynamak vs.</p>
+    </section>
+
+    <a href="https://www.google.com/search?q=satran%C3%A7&sxsrf=ALeKk00t6P7i099lLndJjBa66aWd-w9U_Q:1620240696728&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiN-7b5mrPwAhUJOuwKHSQdCbAQ_AUoAnoECAEQBA&cshid=1620240720095220&biw=1920&bih=937">
+        <img src="https://www.mef.k12.tr/wp-content/uploads/2018/04/satranc-gorseli.jpg" alt="Satranç Resmi" title="Satranç" width="450" height="350">
+    </a>
+
+    <a href="https://www.google.com/search?q=dizi&sxsrf=ALeKk03n1Ha6MqS3PFtOUDMLPLPPklbA1Q:1620240780171&source=lnms&tbm=isch&sa=X&ved=2ahUKEwja25uhm7PwAhWL2aQKHSrhBLMQ_AUoA3oECAEQBQ&biw=1920&bih=937">
+        <img src="https://ceotudent.com/wp-content/images/post/user-5/2jiy4t.jpg" alt="Dizi Resmi" title="Dizi" width="450" height="350">
+    </a>   
+    
+    <a href="https://www.google.com/search?q=film&sxsrf=ALeKk02nR-p8xz8wYHh_k-KkijFtWByTyA:1620240800354&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjp1-uqm7PwAhUJ_aQKHYNFCNMQ_AUoAnoECAEQBA&biw=1920&bih=937">
+        <img src="https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg" alt="Film Resmi" title="Film" width="450" height="350">
+    </a> 
+
+    <a href="https://www.google.com/search?q=futbol&sxsrf=ALeKk01inNNvQHuCncRKnOkOtA7lDpg4Tg:1620240702071&source=lnms&tbm=isch&sa=X&ved=2ahUKEwje_Pz7mrPwAhUKzKQKHdk1BNAQ_AUoAXoECAEQAw&cshid=1620240824272283&biw=1920&bih=937">
+        <img src="https://www.yapbozoyunları.com/imatjes/y%C3%BCksek-h%C4%B1z-ise-kaptan-tsu_4fd09b996e824-p.jpg" alt="Futbol Resmi" title="Futbol" width="450" height="350">
+    </a>
+    
+    <a href="https://www.google.com/search?q=bilgisayar+oyunu&sxsrf=ALeKk01PZFauOCFnWLn5L32Jtnq2qgxK2Q:1620240703664&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiond78mrPwAhXB_qQKHb-CCOMQ_AUoAnoECAEQBA&cshid=1620240846527593&biw=1920&bih=937">
+        <img src="https://teknosafari.net/wp-content/uploads/2020/06/oyun-bilgisayari-alirken-dikkat-edilmesi-gerekenler.jpg" alt="Bilgisayar Oyunu Resmi" title="Bilgisayar Oyunu" width="450" height="350">
+    </a>
+    
+    <a href="https://www.google.com/search?q=masa+tenisi&sxsrf=ALeKk02Qc3a_W12izzH6djdvPVm-AEHJBw:1620240865103&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjTvtvJm7PwAhXQ-qQKHZjlBI4Q_AUoAnoECAIQBA&biw=1920&bih=937">
+        <img src="https://www.trtspor.com.tr/resimler/342000/343804.jpg" alt="Tenis Resmi" title="Tenis" width="450" height="350">
+    </a>
+
+    <a href="https://www.google.com/search?q=kitap&sxsrf=ALeKk03B4qrRQWAUnbvW1NeEU9T0jT-O0g:1620240886012&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiqgNjTm7PwAhWKC-wKHehCA_EQ_AUoAnoECAIQBA&biw=1920&bih=937">
+        <img src="https://cdn.kidega.com/blog/large/%C3%A7ok-satan-kitaplar-De-iY.jpg" alt="Kitap Resmi" title="Kitap" width="450" height="350">
+    </a> 
+    
+    <a href="https://www.google.com/search?q=y%C3%BCzmek&sxsrf=ALeKk00Sb3jFG0iU_jA3gCqDF1VuaHfS3g:1620240903875&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiWhZrcm7PwAhVR2KQKHfHVDGgQ_AUoAXoECAEQAw&biw=1920&bih=937">
+        <img src="https://iasbh.tmgrup.com.tr/2b4b7c/366/218/0/60/1254/806?u=https://isbh.tmgrup.com.tr/sbh/2018/11/23/ruyada-yuzmek-gormek-ne-anlama-gelir-1542963671242.jpg" alt="Yüzme Resmi" title="Yüzme" width="450" height="350">
+    </a> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+  </body>
+</html>
