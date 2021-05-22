d7ff0e68501598cd8f8c73b69b9132459cae13c3
Sercan <ssercanozerr@gmail.com>
2021-05-22 15:58:07 +0300

    bilgi.php

---
+++ bilgi.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Kullanıcı Bilgileri"."<br>";
+    echo "Adı:".$_POST["ad"]."<br>";
+    echo "Soyadı:".$_POST["soyad"]."<br>";
+    echo "Cinsiyeti:".$_POST["cinsiyet"]."<br>";
+    echo "E-mail Adresi:".$_POST["email"]."<br>";
+    echo "Şifresi:".$_POST["sifre"]."<br>";
+    echo "Yaşadığı Ülke:".$_POST["ulke"]."<br>";
+    echo "Yaşadığı Şehir:".$_POST["sehir"]."<br>";
+    echo "Dosyası:".$_POST["dosya"]."<br>";
+    echo "Mesajı:".$_POST["mesaj"]."<br>";
+    echo "Sözleşmeyi Kabul Ettiniz.";
+?>
